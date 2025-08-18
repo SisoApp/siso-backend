2bd2532629e6c22a8f0641844ceac2eb8b4da09e
dlghwns200 <dlghwns200@gmail.com>
2025-08-18 12:15:28 +0900

    chat 엔티티 작성(#29)

---
+++ src/main/java/com/siso/chat/domain/model/ChatMessage.java
@@ -0,0 +1,28 @@
+package com.siso.chat.domain.model;
+
+import com.siso.call.domain.model.Call;
+import com.siso.common.domain.BaseTime;
+import com.siso.user.domain.model.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "chat_messages")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatMessage extends BaseTime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private ChatRoom chatRoomId;
+
+    private User senderId;
+
+    private String content;
+
+    private boolean is_deleted;
+}
