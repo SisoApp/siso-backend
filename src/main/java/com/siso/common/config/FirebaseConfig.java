cef9d02b37b3f93a39ed85441234312f46a294c3
dlghwns200 <dlghwns200@gmail.com>
2025-08-19 20:46:09 +0900

    feat:기본적인 파이어베이스 config, service (#33)

---
+++ src/main/java/com/siso/common/config/FirebaseConfig.java
@@ -0,0 +1,30 @@
+package com.siso.common.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+
+@Configuration
+@RequiredArgsConstructor
+public class FirebaseConfig {
+
+    @Value("${firebase.service-account}")
+    private Resource serviceAccount;
+
+    @PostConstruct
+    public void init() throws IOException {
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream()))
+                    .build();
+            FirebaseApp.initializeApp(options);
+        }
+    }
+}
