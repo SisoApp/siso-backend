c9cb0db3c57d493868113c812b58d064b88a6e25
HyunRe <whwjdqls6868@naver.com>
2025-09-20 23:44:18 +0900

    fix: TaskScheduler 등록

--- src/main/java/com/siso/chat/infrastructure/WebSocketConfig.java
+++ src/main/java/com/siso/chat/infrastructure/WebSocketConfig.java
@@ -1,9 +1,12 @@
 package com.siso.chat.infrastructure;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.*;
 
 @RequiredArgsConstructor
@@ -11,6 +14,15 @@ import org.springframework.web.socket.config.annotation.*;
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     private final JwtChannelInterceptor jwtChannelInterceptor;
+
+    @Bean
+    public TaskScheduler messageBrokerTaskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("wss-heartbeat-");
+        scheduler.initialize();
+        return scheduler;
+    }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -27,7 +39,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic", "/queue").setHeartbeatValue(new long[]{10000, 10000});; // 구독 prefix 활성화 + 10초마다 heartbeat
+        registry.enableSimpleBroker("/topic", "/queue") // 구독 prefix
+                .setHeartbeatValue(new long[]{10000, 10000})             // 활성화 + 10초마다 heartbeat
+                .setTaskScheduler(messageBrokerTaskScheduler());         // TaskScheduler
         registry.setApplicationDestinationPrefixes("/app"); // 클라 -> 서버 보낼 때 prefix
         registry.setUserDestinationPrefix("/user"); // 개인 메시지(1:1 메시지용) prefix
     }
