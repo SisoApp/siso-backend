beaffee4a156ecc60a76cbe4d46d269bcd3badb3
HyunRe <whwjdqls6868@naver.com>
2025-09-22 23:46:06 +0900

    fix:WebSocketConfig

--- src/main/java/com/siso/chat/infrastructure/WebSocketConfig.java
+++ src/main/java/com/siso/chat/infrastructure/WebSocketConfig.java
@@ -31,14 +31,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-stomp")
-                .setAllowedOriginPatterns("http://13.124.11.3:8080")
-                .withSockJS(); // 필요하면 Fallback
+                .setAllowedOriginPatterns("http://13.124.11.3:8080");
     }
 
     @Override
     public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
-        registration.setSendTimeLimit(15_000)               // 메시지 전송 최대 시간 (15초)
-                .setSendBufferSizeLimit(512 * 1024)     // 버퍼 크기 (512KB)
+        registration
+                .setMessageSizeLimit(128 * 1024)        // 개별 메시지 최대 128KB
+                .setSendBufferSizeLimit(512 * 1024)    // 버퍼 사이즈 512KB
+                .setSendTimeLimit(20_000)              // 20초까지 전송 허용
                 .addDecoratorFactory(handler -> new WebSocketHandlerDecorator(handler) {
                     @Override
                     public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
@@ -51,7 +52,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic", "/queue") // 구독 prefix
-                .setHeartbeatValue(new long[]{20000, 20000})             // 활성화 + 20초마다 heartbeat
+                .setHeartbeatValue(new long[]{10_000, 10_000})             // 활성화 + 10초마다 heartbeat
                 .setTaskScheduler(wsHeartbeatScheduler());         // TaskScheduler
         registry.setApplicationDestinationPrefixes("/app"); // 클라 -> 서버 보낼 때 prefix
         registry.setUserDestinationPrefix("/user"); // 개인 메시지(1:1 메시지용) prefix
@@ -59,6 +60,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration
+                .taskExecutor()
+                .corePoolSize(4)    // 동시에 처리할 최소 스레드 수
+                .maxPoolSize(8)    // 최대 스레드 수
+                .queueCapacity(500) // 대기열
+                .keepAliveSeconds(60);
         registration.interceptors(jwtChannelInterceptor);
     }
 }
